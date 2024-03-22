b5c3cae7b1a5f728d083ad126adae19daa87d48b
THG20203 <tristanhgriffiths8@yahoo.com>
2024-03-22 15:42:51 +0000

    block scope in printAge

--- JS Behind the Scenes/scriptbehindscenes.js
+++ JS Behind the Scenes/scriptbehindscenes.js
@@ -8,15 +8,21 @@ const calcAge = (birthYear) => {
   const age = 2023 - birthYear;
 
   function printAge() {
-    const output = `You are the ${age}, born in ${birthYear}`;
+    const output = `${firstName} are the ${age}, born in ${birthYear}`;
     console.log(output);
+
+    /* block scope in printAge function */
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const string = `Oh, and you're a millenial, ${firstName}`;
+      console.log(string);
+    }
   }
   /* have to call printAge() inside calcAge cause printAge is a local function */
   printAge();
   return age;
 };
 
-/* Globa variable */
+/* Global variable */
 const firstName = "Tristan";
 
 calcAge(1991);
