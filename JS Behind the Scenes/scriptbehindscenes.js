7a6b8acc2f1a3c5f5e291e13d47f379a6715c686
THG20203 <tristanhgriffiths8@yahoo.com>
2024-03-22 22:15:12 +0000

    this keyword with arrow functions

--- JS Behind the Scenes/scriptbehindscenes.js
+++ JS Behind the Scenes/scriptbehindscenes.js
@@ -9,12 +9,12 @@ const calcAge = (birthYear) => {
 
   function printAge() {
     const output = `${firstName} are the ${age}, born in ${birthYear}`;
-    console.log(output);
+    // console.log(output);
 
     /* block scope in printAge function */
     if (birthYear >= 1981 && birthYear <= 1996) {
       const string = `Oh, and you're a millenial, ${firstName}`;
-      console.log(string);
+      //  console.log(string);
     }
   }
   /* have to call printAge() inside calcAge cause printAge is a local function */
@@ -29,18 +29,18 @@ calcAge(1991);
 
 /* HOISTING */
 /* hoisting with variables */
-console.log(me); // undefined - var values hoisted to undefined
-console.log(job); // cannot access job before initialization
-console.log(year); // cannot access year before initialization
+//console.log(me); // undefined - var values hoisted to undefined
+//console.log(job); // cannot access job before initialization
+//console.log(year); // cannot access year before initialization
 
 var me = "Jonas";
 let job = "teacher";
 const year = 1991;
 
 /* hoisting with functions */
-console.log(addDeclr(2, 3)); // get 5 -> can call function declaration before defined in code
-console.log(addExpr(2, 3)); // cannot access addExpr before initialisation
-console.log(arrow(2, 3)); // cannot access addExpr before initialisation
+//console.log(addDeclr(2, 3)); // get 5 -> can call function declaration before defined in code
+//console.log(addExpr(2, 3)); // cannot access addExpr before initialisation
+//console.log(arrow(2, 3)); // cannot access addExpr before initialisation
 
 /* declaration */
 function addDeclr(a, b) {
@@ -62,7 +62,7 @@ if (!numProducts) deleteShoppingCart;
 var numProducts = 10;
 
 function deleteShoppingCart() {
-  console.log("All products deleted"); // get all products deleted despite numProducts = 10
+  //  console.log("All products deleted"); // get all products deleted despite numProducts = 10
 }
 
 /* Why cause if (!numProducts) deleteShoppingCart; was before var numProducts = 10 */
@@ -74,8 +74,24 @@ let y = 2; // WON'T create property on window object
 const z = 3; // WON'T create property on window object
 
 /* proof? */
-console.log(x === window.x); // true
-console.log(y === window.y); // false
-console.log(z === window.z); // false
+// console.log(x === window.x); // true
+// console.log(y === window.y); // false
+// console.log(z === window.z); // false
 
 /* THIS KEYWORD */
+console.log(this); // undefined cause in strict mode
+
+/* regular function */
+const calcAge2 = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAge2(1991); // regular function call  - cause the function call doesn't have an owner
+
+/* arrow function - uses this keyword of parent scope - so its window for arrow - 
+cause thats the this keyword in the global scope. */
+const calcAgeArrow = (birthYear) => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1980);
