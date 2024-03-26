51e0f490462ee26e97b3049d6d6e6c0849cf4115
THG20203 <tristanhgriffiths8@yahoo.com>
2024-03-26 16:27:49 +0000

    beginning spread operator work

--- Data Structures, Modern Operators and Strings/SpreadOperator/spreadoperator.js
+++ Data Structures, Modern Operators and Strings/SpreadOperator/spreadoperator.js
@@ -1,1 +1,8 @@
-alert("Hello Spread");
+/* Spread operator - expand array into all its elements- so unpacking all the 
+array elements at once */
+const arr = [7, 8, 9];
+
+/* want to create new array based on this array - but with some new 
+elements at the beginning (How would we do that?) */
+const newArr = [1, 2, ...arr];
+console.log(newArr); // (5) [1, 2, 7, 8, 9]
