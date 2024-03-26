fd1132510e0646e6ce58ce8071b7f3acf5863fc6
THG20203 <tristanhgriffiths8@yahoo.com>
2024-03-26 00:06:24 +0000

    adding opening hours nested object into restaurant

--- Data Structures, Modern Operators and Strings/Destructuring/destructuring.js
+++ Data Structures, Modern Operators and Strings/Destructuring/destructuring.js
@@ -10,6 +10,20 @@ const restaurant = {
   categories: ["Italian", "Pizzaeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
   order: function (starterIndex, mainIndex) {
     return [this.startMenu[starterIndex], this.mainMenu[mainIndex]];
   },
@@ -100,3 +114,6 @@ const obj = { a: 23, b: 7, c: 14 };
 interpreted by JavaScript is a code block when you start a line like this */
 ({ a, b } = obj);
 console.log(a, b); // 23 7
+
+/* nested objects - create two variables open and close and these should 
+contain the open and close hours for Friday */
