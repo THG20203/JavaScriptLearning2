7eb53e13027c40aff5de3cf060834e1e811f60ec
THG20203 <tristanhgriffiths8@yahoo.com>
2024-03-27 16:51:26 +0000

    small amount of rest theory

--- Data Structures, Modern Operators and Strings/Rest Pattern and Parameters/restpattern.js
+++ Data Structures, Modern Operators and Strings/Rest Pattern and Parameters/restpattern.js
@@ -9,3 +9,8 @@ console.log(arr); // (4) [1, 2, 3, 4]
 /* REST, because on left side of = */
 const [a, b, ...others] = [1, 2, 3, 4, 5];
 console.log(a, b, others); // 1 2 (3) [3, 4, 5]
+
+/* Above whats happening? First and second elements become first and second variables.
+then ...others */
+/* ...others is the rest pattern, because will take the rest of the elements (remaining
+elements of the array) and put them into a new array (in this case we call them others). */
