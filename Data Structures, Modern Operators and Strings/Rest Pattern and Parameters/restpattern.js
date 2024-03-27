f721e84fe2f3d7afd1cc7ec59f002a3ddb11d72c
THG20203 <tristanhgriffiths8@yahoo.com>
2024-03-27 16:46:53 +0000

    spread vs rest - console.log

--- Data Structures, Modern Operators and Strings/Rest Pattern and Parameters/restpattern.js
+++ Data Structures, Modern Operators and Strings/Rest Pattern and Parameters/restpattern.js
@@ -4,3 +4,8 @@
 
 /* SPREAD, because on right side of = */
 const arr = [1, 2, ...[3, 4]];
+console.log(arr); // (4) [1, 2, 3, 4]
+
+/* REST, because on left side of = */
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others); // 1 2 (3) [3, 4, 5]
