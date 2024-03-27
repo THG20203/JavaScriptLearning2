924a8fcec58a9360f29144f3fdca8bbdeb050ca5
THG20203 <tristanhgriffiths8@yahoo.com>
2024-03-27 16:40:50 +0000

    rest and spread

---
+++ Data Structures, Modern Operators and Strings/Rest Pattern and Parameters/restpattern.js
@@ -0,0 +1,6 @@
+/* Rest Pattern, collect multiple elements -> condense them into an array */
+
+/* Spread is to unpack an array, rest is to pack up an array */
+
+/* SPREAD, because on right side of = */
+const arr = [1, 2, ...[3, 4]];
