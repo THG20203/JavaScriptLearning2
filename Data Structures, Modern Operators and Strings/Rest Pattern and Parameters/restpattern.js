71d3b018cbacb172819784faf248e6150e4c3211
THG20203 <tristanhgriffiths8@yahoo.com>
2024-03-28 15:07:30 +0000

    continuing rest operator calculations

--- Data Structures, Modern Operators and Strings/Rest Pattern and Parameters/restpattern.js
+++ Data Structures, Modern Operators and Strings/Rest Pattern and Parameters/restpattern.js
@@ -15,6 +15,9 @@ then ...others */
 /* ...others is the rest pattern, because will take the rest of the elements (remaining
 elements of the array) and put them into a new array (in this case we call them others). */
 
+/* Console.log of above will produce arrays. Rest takes multiple numbers and then packs 
+them all into one array. Opposite of spread operator. */
+
 /* Rest pattern collects the elements that area unused in the destructuring assingment
 but after the last element used, so the REST of the elements */
 
@@ -24,13 +27,18 @@ destructuring assignment */
 // REST and functions
 /* rest parameters */
 const add = function (...numbers) {
+  /* sum variable to be accumulating */
   let sum = 0;
   /* we can do i < numbers.length cause numbers is an array */
-  for (i = 0, i < numbers.length; i++) {}
+  for (i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
 };
-/* Console.log of above will produce arrays. Rest takes multiple numbers and then packs 
-them all into one array. Opposite of spread operator. */
 
-add(2, 3);
-add(5, 3, 7, 2);
-add(8, 11, 6, 5, 3, 7, 2);
+/* equivalent to sum = sum + numbers[i];. In this case, it adds the value of 
+numbers[i] to the current value of sum and updates sum with the result. */
+
+console.log(add(2, 3)); // 5
+console.log(add(5, 3, 7, 2)); // 17
+console.log(add(8, 11, 6, 5, 3, 7, 2)); // 42
