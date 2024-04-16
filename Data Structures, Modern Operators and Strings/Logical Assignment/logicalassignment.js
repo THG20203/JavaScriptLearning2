9655ef28802887c38b819c013952b4c68d5fa7dd
THG20203 <tristanhgriffiths8@yahoo.com>
2024-04-16 21:45:28 +0100

    working further with logical operators

--- Data Structures, Modern Operators and Strings/Logical Assignment/logicalassignment.js
+++ Data Structures, Modern Operators and Strings/Logical Assignment/logicalassignment.js
@@ -9,9 +9,20 @@ const rest2 = {
   owner: "Giovanni Rossi",
 };
 
-rest1.numGuests = rest1.numGuests || 10;
+// rest1.numGuests = rest1.numGuests || 10;
 // 20 - numGuests for rest2 must be true, otherwise 10 returned
-console.log(rest1); // {name: "Capri", numGuests: 20}
+// console.log(rest1); // {name: "Capri", numGuests: 20}
 
-rest2.numGuests = rest2.numGuests || 10;
-console.log(rest2); // {name: "La Piazaa", owner: "Giovanni Rossi", numGuests: 10}
+// rest2.numGuests = rest2.numGuests || 10;
+// 10 because of short circuiting, rest2 numGuests undefined so 10 returned
+// console.log(rest2); // {name: "La Piazaa", owner: "Giovanni Rossi", numGuests: 10}
+
+// OR operator - write same thing in more concise way.
+/* Or assigns value to a variable if that value is falsy */
+
+rest1.numGuests ||= 10; // 20 - cause 20 was already there
+rest2.numGuests ||= 10; // 10 cause there was nothing there - rest2.numGuests was falsy value
+
+/* If numGuests in rest1 changed to 0? - use nullish logical assingment */
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
